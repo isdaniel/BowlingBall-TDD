a9fd14ff7f9d8e072e9caafc88f5945ffe89c083
Daniel.Shih <dog830228@gmail.com>
2019-06-10 21:38:58 +0800

    add test

---
+++ BowlingBall/BowlingBall.Tests/BowlingGameTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BowlingBall.Tests
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        [Test]
+        public void All_PinEmpty_Score0()
+        {
+
+            BowlingGame game = new BowlingGame();
+
+            int expectScore = 0;
+
+            foreach (var s in Enumerable.Repeat(0, 21))
+            {
+                game.Roll(s);
+            }
+
+            Assert.AreEqual(expectScore, game.Score);
+        }
+
+        [TestCase(new [] { 8, 2, 8, 1, 5, 5 },29)]
+        [TestCase(new [] { 10, 10, 10 }, 60)]
+        [TestCase(new [] { 10, 10 }, 30)]
+        [TestCase(new[]  { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }, 300)]
+        public void PinNormal_Scores_is_29(int[] pins,int expectScore)
+        {
+            BowlingGame game = new BowlingGame();
+
+            foreach (var pin in pins)
+            {
+                game.Roll(pin);
+            }
+
+            Assert.AreEqual(expectScore,game.Score);
+        }
+
+    }
+}
